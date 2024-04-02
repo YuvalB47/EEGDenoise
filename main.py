d010e3f92954ec6c12455d1e513b0a7f6d24929b
YuvalDormotech <121929367+YuvalDormotech@users.noreply.github.com>
2024-04-02 15:27:30 +0300

    first push

---
+++ main.py
@@ -0,0 +1,21 @@
+import os, sys 
+
+import numpy as np 
+import scipy 
+import matplotlib.pyplot as plt
+
+# import librosa 
+
+
+def main():
+
+    data = scipy.io.loadmat(r"DataSet\G1D1D2\G1D1.mat")
+
+    sa = data["Sa"]    
+
+    x = 0
+    pass 
+
+
+if __name__ == "__main__":
+    main()
